02da49a0ddc948a65d4b80ed0786d1513f6163dc
xtcedro <pedro.dfedro@gmail.com>
2025-04-04 13:17:27 -0500

    update

--- utils/createAdminUser.js
+++ utils/createAdminUser.js
@@ -20,10 +20,10 @@ async function createAdmin() {
     const hash = await bcrypt.hash(password, 12);
 
     const db = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: process.env.ADMIN_DB_HOST,
+      user: process.env.ADMIN_DB_USER,
+      password: process.env.ADMIN_DB_PASSWORD,
+      database: process.env.ADMIN_DB_NAME,
     });
 
     await db.execute(
