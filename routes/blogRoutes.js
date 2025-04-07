991a9783be68268f55cbaee061f87d8b04bf1d0a
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-04-07 13:50:01 -0500

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,18 +1,14 @@
 import express from "express";
-import { getAllBlogs, createBlogPost } from "../controllers/blogController.js";
+import {
+  getAllBlogs,
+  createBlogPost,
+  updateBlogPost
+} from "../controllers/blogController.js";
 
 const router = express.Router();
 
-// GET /api/blogs
-router.get("/", (req, res, next) => {
-  console.log("📡 Route: GET /api/blogs");
-  getAllBlogs(req, res, next);
-});
-
-// POST /api/blogs
-router.post("/", (req, res, next) => {
-  console.log("🚀 Route: POST /api/blogs");
-  createBlogPost(req, res, next);
-});
+router.get("/", getAllBlogs);
+router.post("/", createBlogPost);
+router.put("/:id", updateBlogPost); // ✅ New PUT route
 
 export default router;
