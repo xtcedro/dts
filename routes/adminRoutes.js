6d45ac6f2029aff6dfe3dec7fabd2aed1f5878be
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-04-06 21:25:09 -0500

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,6 +3,9 @@ import { adminLogin } from "../controllers/adminController.js";
 
 const router = express.Router();
 
-router.post("/login", adminLogin);
+router.post("/login", (req, res, next) => {
+  console.log(`🛂 /api/admin/login hit at ${new Date().toISOString()}`);
+  next(); // Pass control to adminLogin
+}, adminLogin);
 
 export default router;
