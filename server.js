128af157405725c9ae827f2181811dcf0995055a
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-04-07 13:40:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import stripeRoutes from "./routes/stripe.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 import settingsRoutes from "./routes/settingsRoutes.js";
+import blogRoutes from "./routes/blogRoutes.js";
 
 dotenv.config();
 const app = express();
